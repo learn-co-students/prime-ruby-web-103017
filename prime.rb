0304f6785448225fdbfaf5f7ceab632bf48d4704
Yuliya Yasenetska <jasenetska@gmail.com>
2017-10-10 02:16:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  if integer<=1
+   return false
+  end
+  (2..integer-1).each do |divisor|
+     puts "#{integer} #{divisor}"
+      if (integer % divisor == 0)
+       return false
+      end
+  end
+  return true
+end
