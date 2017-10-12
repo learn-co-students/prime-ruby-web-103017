65730aebd863e3881fad8f2433e91ced816a3a7b
github username <github email address>
2017-10-11 23:20:53 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
+def prime?(int)
+  if int < 2
+    return false
+  elsif int <= 3
+    return true
+  else
+    array = *(2...int)
+    array.each do |div|
+      if int % div == 0
+        return false
+      end
+    end
+    return true
+  end
+end
+
 # Add  code here!
+
+puts prime?(1763)
