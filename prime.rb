43fe13cd4e549503c99fed04c6bf26a7f43120dc
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-13 17:32:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+
+  notPrime = 1
+  for each in (2..number)
+    if each % number == 0
+      notPrime += 1
+    end
+  end
+
+  if notPrime > 1
+    return false
+  else
+    return true
+end
