535e4816813786a65e0eaccb272e3dec8a2b5fb4
Silvia Chung <schung97@gmail.com>
2017-10-13 23:57:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  else
+    # range = (2..(number - 1)).to_a
+    (2..(number - 1)).each do |x|
+      if number % x == 0
+        return false
+      end
+    end
+  return true
+  end
+end
+
+# def prime?(number)
+#   if number < 2
+#     return false
+#   else
+#     # range = (2..(number - 1)).to_a
+#     (2..(number - 1)).each {|x| number % x == 0, return false}
+#     return true
+#   end
+# end
