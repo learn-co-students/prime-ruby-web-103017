8a600c617b2ed2f0423a1757ee91f39c220c199f
Erick Camacho <erickcamacho1@gmail.com>
2017-10-18 18:39:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+    if num <= 1
+        return false
+    end
+    (2..num-1).each do |x|
+        if num % x == 0
+            return false
+        end
+    end
+    return true
+end
